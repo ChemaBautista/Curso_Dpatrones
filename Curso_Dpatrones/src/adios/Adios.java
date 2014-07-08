ee8cf5feff22f197e8887c309a435dde85ca112f
ch3ma <ch3ma@C0d3r>
2014-07-07 23:04:46 -0500

    Se agregan nuevas lineas y se vera si se agrega en el historial

--- Curso_Dpatrones/src/adios/Adios.java
+++ Curso_Dpatrones/src/adios/Adios.java
@@ -4,7 +4,9 @@ public class Adios {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("adios mundo");
-	
+	// vamos a agregar otro System.out.print();
+		
+		System.out.print("aqui veremos si se agragan los cambios en el hisorial");
 
 }
 }
