2c715aaeb47ae67be598383090fcc7c9cc35ca11
ch3ma <ch3ma@C0d3r>
2014-07-07 22:30:16 -0500

    Hola mundo

---
+++ Curso_Dpatrones/src/Hola/Hello.java
@@ -0,0 +1,13 @@
+package Hola;
+
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("hola mundo");
+	}
+
+}
